baebd6fbe8807b75f416c18da636134ef5417a20
Safariminer <74744603+Safariminer@users.noreply.github.com>
2022-12-11 17:17:44 -0500

    Finished first working version of norloges

--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
 # \\\\\ 3BUNE
 3BUNE is a fully-featured tribune complete with its bouchot and coincoin.
 
+## Uses:
+- jQuery
+- PHP 8
+- This picture from Pixabay: https://pixabay.com/fr/photos/pissenlit-prairie-prairie-pissenlit-3382663/
+
 ## To-do
 ### Bouchot
 - Posting system
--- src/frontend.php
+++ src/frontend.php
@@ -8,7 +8,7 @@ function TreatString($message): string{
     if(stripos($message, "tag:") === 0){
         $finalmessage = substr($message, 0, 22);
         $tag = $finalmessage;
-        $finalmessage = "tag:<span id=\"" . substr($tag, 4, 14);
+        $finalmessage = "tag:<span class=\"id" . substr($tag, 4, 14);
         $finalmessage = $finalmessage . "\" mouseover=\"TimeLookup()\" onclick=\"GetTime()\">" . substr($tag, 4, 14);
         $finalmessage = $finalmessage . "</span> ---";
         $finalmessage = $finalmessage . substr($message, 22);
@@ -23,7 +23,7 @@ $messagelist->load('backend.xml');
 $messages = $messagelist->getElementsByTagName('post');
 
 foreach($messages as $message){
-    echo "<time mouseover=\"TimeLookup()\" onclick=\"GetTime()\" id=\"" . $message->getAttribute('time') . "\">" . $message->getAttribute('time') . "</time> | " . htmlspecialchars($message->childNodes->item(0)->textContent) . " : " . TreatString($message->childNodes->item(1)->textContent) . "<br/>";
+    echo "<time mouseover=\"TimeLookup()\" onclick=\"GetTime()\" class=\"id" . $message->getAttribute('time') . "\">" . $message->getAttribute('time') . "</time> | " . htmlspecialchars($message->childNodes->item(0)->textContent) . " : " . TreatString($message->childNodes->item(1)->textContent) . "<br/>";
 }
 
 ?>
