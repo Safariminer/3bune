6d9ffeef98fe87b92dec8e4241b293d6cc665788
Safariminer <74744603+Safariminer@users.noreply.github.com>
2022-12-23 11:18:37 -0500

    Fixed bugs concerning totoz and moments

--- src/frontend.php
+++ src/frontend.php
@@ -10,9 +10,9 @@ function TreatString($message): string{
     );
     
     $finalmessage = str_ireplace(htmlentities($_REQUEST["ua"] . "<"), "<span style=\"background-color: red;\">" . $_REQUEST["ua"] . htmlentities("< ") . "</span>", $finalmessage);
-    $finalmessage = preg_replace("/==&gt;[\s|&#xA0;]([0-z\s]+)[\s|&#xA0;]&lt;==/", "<b style=\"color:yellow\">==&gt; $1 &lt;==</b>", $finalmessage);
+    $finalmessage = preg_replace("/==&gt;[\s|&#xA0;]([0-z\s&gt;&lt;]+)[\s|&#xA0;]&lt;==/", "<b style=\"color:yellow\">==&gt; $1 &lt;==</b>", $finalmessage);
     $finalmessage = preg_replace("/\">==&gt; nbsp;/", "\">==&gt; ", $finalmessage);
-    $finalmessage = preg_replace("/\[:([0-z\s]+)\]/", "<img src=\"https://totoz.eu/img/$1\" title=\"$1\" onclick=\"alert('Looking at: [:$1]')\">", $finalmessage);
+    $finalmessage = preg_replace("/\[:([^\]]+)\]/", "<img src=\"https://totoz.eu/img/$1\" title=\"$1\" onclick=\"alert('Looking at: [:$1]')\">", $finalmessage);
     
     // $finalmessage = str_replace("[:", "<img src=\"https://totoz.eu/img/", $finalmessage);
     
